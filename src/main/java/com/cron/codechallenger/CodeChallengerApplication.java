46b0ac5206bf7ac7b8a1230bf1b6126ca9a3c34f
ogo okafor <ogo.okafor@turnkeyafrica.com>
2022-02-13 10:46:41 +0100

    initial commit

---
+++ src/main/java/com/cron/codechallenger/CodeChallengerApplication.java
@@ -0,0 +1,13 @@
+package com.cron.codechallenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeChallengerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeChallengerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cron/codechallenger/CodeChallengerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cron.codechallenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeChallengerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
