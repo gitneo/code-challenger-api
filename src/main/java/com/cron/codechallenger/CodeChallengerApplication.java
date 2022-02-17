31e1ec06136d657fbe61fd987cdb67c04ec77292
Ogo Okafor <ogookafor@gmail.com>
2022-02-17 17:04:23 +0100

    Enable jpa repostories

--- src/main/java/com/cron/codechallenger/CodeChallengerApplication.java
+++ src/main/java/com/cron/codechallenger/CodeChallengerApplication.java
@@ -1,9 +1,12 @@
 package com.cron.codechallenger;
 
+import com.cron.codechallenger.data.repositories.PlayerRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackageClasses = PlayerRepository.class)
 public class CodeChallengerApplication {
 
 	public static void main(String[] args) {
